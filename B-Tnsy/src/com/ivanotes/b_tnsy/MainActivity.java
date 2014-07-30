8e32ade0cf23d0ea24ef68d90aa0baa68ab20cf0
ivanbaug <ivanbgp@gmail.com>
2014-07-29 22:49:17 -0500

    Added ConnectActivity, Bluetooth permissions, and some minor layout changes TODO: complete the start activity for result ConnectActivity, also complete the "bluetooth service" to handle all the bluetooth actions

--- B-Tnsy/src/com/ivanotes/b_tnsy/MainActivity.java
+++ B-Tnsy/src/com/ivanotes/b_tnsy/MainActivity.java
@@ -2,14 +2,20 @@ package com.ivanotes.b_tnsy;
 
 import java.util.Locale;
 
+import com.example.android.BluetoothChat.R;
+
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -17,9 +23,26 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity implements
 		ActionBar.TabListener {
+	
+	// Debugging
+    private static final String TAG = "MainActivity";
+    private static final boolean D = true;
+    //It is used like this: if(D) Log.d(TAG, "My debug message");
+    
+    // Name of the connected device
+    private String mConnectedDeviceName = null;
+     // Local Bluetooth adapter
+    private BluetoothAdapter mBluetoothAdapter = null;
+    
+    // Intent request codes
+    private static final int REQUEST_CONNECT_DEVICE_SECURE = 1;
+    private static final int REQUEST_CONNECT_DEVICE_INSECURE = 2;
+    private static final int REQUEST_ENABLE_BT = 3;
+    private static final int REQUEST_CONNECT_DEVICE_OTHER = 4;
 
 	/**
 	 * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -74,6 +97,8 @@ public class MainActivity extends ActionBarActivity implements
 					.setText(mSectionsPagerAdapter.getPageTitle(i))
 					.setTabListener(this));
 		}
+		
+		
 	}
 
 	@Override
@@ -89,7 +114,14 @@ public class MainActivity extends ActionBarActivity implements
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
-		if (id == R.id.action_settings) {
+		if (id == R.id.action_connect) {
+			Intent intent = new Intent(this, ConnectActivity.class);
+			startActivityForResult(intent, REQUEST_CONNECT_DEVICE_OTHER);
+            return true;
+		}else if (id == R.id.action_settings) {
+			Toast toast = Toast.makeText(getApplicationContext(), 
+					R.string.toast_txt_no_settings, Toast.LENGTH_SHORT);
+			toast.show();
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
@@ -184,5 +216,34 @@ public class MainActivity extends ActionBarActivity implements
 			return rootView;
 		}
 	}
+	
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(D) Log.d(TAG, "onActivityResult " + resultCode);
+        switch (requestCode) {
+        case REQUEST_CONNECT_DEVICE_SECURE:
+            // When DeviceListActivity returns with a device to connect
+            if (resultCode == Activity.RESULT_OK) {
+                connectDevice(data, true);
+            }
+            break;
+        case REQUEST_CONNECT_DEVICE_INSECURE:
+            // When DeviceListActivity returns with a device to connect
+            if (resultCode == Activity.RESULT_OK) {
+                connectDevice(data, false);
+            }
+            break;
+        case REQUEST_ENABLE_BT:
+            // When the request to enable Bluetooth returns
+            if (resultCode == Activity.RESULT_OK) {
+                // Bluetooth is now enabled, so set up a chat session
+                setupChat();
+            } else {
+                // User did not enable Bluetooth or an error occurred
+                Log.d(TAG, "BT not enabled");
+                Toast.makeText(this, R.string.bt_not_enabled_leaving, Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
 
 }
